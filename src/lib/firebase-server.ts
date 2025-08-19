a46705b0a2292081921e897e6515d9ec11126c39
ElFerlo (ElFerloTV) <ferangaritam@gmail.com>
2025-08-19 02:46:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-server.ts
+++ src/lib/firebase-server.ts
@@ -1,6 +1,4 @@
 
-'use server';
-
 import { initializeApp, getApps, App, getApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
