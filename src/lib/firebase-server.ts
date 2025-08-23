8ef42bd51378cba1ec747d83f4f6d28d075c3ea3
ElFerlo (ElFerloTV) <ferangaritam@gmail.com>
2025-08-23 02:17:31 +0000

    eliminar la directiva "use server"

--- src/lib/firebase-server.ts
+++ src/lib/firebase-server.ts
@@ -1,4 +1,3 @@
-'use server';
 import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
