74e02d61b6ef126ae23bbe3c137cfe4b640191e9
ElFerlo (ElFerloTV) <ferangaritam@gmail.com>
2025-08-23 01:53:12 +0000

    Invalid "use server" Value Why This Error Occurred This error occurs whe

--- Users/ferangarita/Documents/8. MUWISE/Muwise-V2.0/src/lib/firebase-server.ts
+++ Users/ferangarita/Documents/8. MUWISE/Muwise-V2.0/src/lib/firebase-server.ts
@@ -1,4 +1,3 @@
-
 import { initializeApp, getApps, App, cert, ServiceAccount } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
